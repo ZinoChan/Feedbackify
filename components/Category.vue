50cbb67140b68941577ee48b4e8b2b4b227dce38
ZinoChan <devzinochan@gmail.com>
2023-01-07 22:22:09 +0100

    basic category component

---
+++ components/Category.vue
@@ -0,0 +1,12 @@
+<template>
+  <span
+    :class="category"
+    class="text-xs font-bold capitalize bg-opacity-10 mr-2 px-3 py-1.5 rounded-md border"
+  >
+    {{ category }}
+  </span>
+</template>
+
+<script setup>
+const { category } = defineProps(["category"]);
+</script>
