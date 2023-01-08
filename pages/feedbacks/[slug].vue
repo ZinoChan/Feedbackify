d04190554260c615375f6263ec5fd71fb61e6211
ZinoChan <devzinochan@gmail.com>
2023-01-08 13:40:20 +0100

    feedback page basic ui

---
+++ pages/feedbacks/[slug].vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="max-w-screen-lg mx-auto px-4 y-6">
+    <div class="items-start grid grid-cols-4 gap-8">
+      <div>
+        <ProductCard />
+        <CategoryList />
+      </div>
+      <div class="col-span-3">
+        <FeedbackHeader />
+        <FeedBackCard />
+        <FeedBackCard />
+        <FeedBackCard />
+        <FeedBackCard />
+      </div>
+    </div>
+  </div>
+</template>
