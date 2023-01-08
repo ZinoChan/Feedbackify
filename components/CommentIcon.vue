a302878efcdbaaaaec205d1cbdb63f438fa2462a
ZinoChan <devzinochan@gmail.com>
2023-01-08 13:39:00 +0100

    add CommentIcon

---
+++ components/CommentIcon.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex space-x-2 items-center">
+    <Icon class="text-xl text-gray-500" name="ant-design:comment-outlined" />
+    <span class="font-bold text-sm">4</span>
+  </div>
+</template>
