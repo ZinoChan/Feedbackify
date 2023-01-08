f4fc6c8447668bb85c0a3c5a05bbb3847171628c
ZinoChan <devzinochan@gmail.com>
2023-01-08 13:39:44 +0100

    add ProductCard

---
+++ components/ProductCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="shadow-md shadow-teal-200 mb-6 rounded-md p-6 bg-gradient-to-r from-green-400 to-teal-400 text-white flex items-end"
+  >
+    <div>
+      <h3 class="font-bold text-xl mb-4">Lapino</h3>
+      <span>feed back board</span>
+    </div>
+  </div>
+</template>
