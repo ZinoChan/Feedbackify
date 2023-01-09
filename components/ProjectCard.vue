88dc48c7777172e10cbc6865db8ccd49d836bc60
ZinoChan <devzinochan@gmail.com>
2023-01-09 18:25:32 +0100

    ProjectCard component

---
+++ components/ProjectCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="bg-white p-6 rounded-md shadow border border-t-4 border-t-cyan-400"
+  >
+    <h3 class="font-bold text-2xl uppercase text-gray-600 mb-4">Lapino</h3>
+    <p class="mb-4 text-sm text-gray-400">
+      an ecommerce web app for kids products.
+    </p>
+    <div class="flex flex-wrap mb-6 space-x-4">
+      <span
+        class="p-2 rounded-full text-xs uppercase bg-teal-50 text-teal-400 font-bold"
+      >
+        Reactjs
+      </span>
+      <span
+        class="p-2 rounded-full text-xs uppercase bg-teal-50 text-teal-400 font-bold"
+      >
+        Node js
+      </span>
+      <span
+        class="p-2 rounded-full text-xs uppercase bg-teal-50 text-teal-400 font-bold"
+      >
+        tailwindcss
+      </span>
+    </div>
+    <div class="flex justify-between items-center">
+      <div class="flex space-x-3 text-sm font-bold text-gray-600">
+        <span>34</span>
+        <span>feedbacks</span>
+      </div>
+      <div class="flex items-center text-xl text-gray-600 space-x-3">
+        <Icon class="hover:text-gray-900" name="ant-design:github-outlined" />
+        <Icon class="hover:text-cyan-500" name="ci:external-link" />
+      </div>
+    </div>
+  </div>
+</template>
