6456c14489982f49a6a4f759c591791d0d060f35
ZinoChan <devzinochan@gmail.com>
2023-01-09 18:25:17 +0100

    styles

--- components/FeedbackHeader.vue
+++ components/FeedbackHeader.vue
@@ -1,18 +1,22 @@
 <template>
-  <nav
-    class="mb-6 bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600"
-  >
-    <div class="container flex flex-wrap items-center justify-between mx-auto">
-      <div class="flex space-x-2 items-center">
-        <Icon class="text-2xl text-gray-900" name="ri:lightbulb-flash-fill" />
-        <span
-          class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
-          >6 suggestions</span
-        >
+  <div>
+    <nav
+      class="mb-6 bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600"
+    >
+      <div
+        class="container flex flex-wrap items-center justify-between mx-auto"
+      >
+        <div class="flex space-x-2 items-center">
+          <Icon class="text-2xl text-gray-900" name="ri:lightbulb-flash-fill" />
+          <span
+            class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+            >6 suggestions</span
+          >
+        </div>
+        <div class="flex md:order-2">
+          <button type="button" class="btn-primary">add feedback</button>
+        </div>
       </div>
-      <div class="flex md:order-2">
-        <button type="button" class="btn-primary">Get started</button>
-      </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
