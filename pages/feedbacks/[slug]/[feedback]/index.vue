cd52df92809a00fa06b7509371152d9ef0d1fed3
ZinoChan <devzinochan@gmail.com>
2023-01-20 20:59:55 +0100

    add ui

--- pages/feedbacks/[slug]/[feedback]/index.vue
+++ pages/feedbacks/[slug]/[feedback]/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-screen-lg mx-auto">
+  <div class="max-w-screen-md mx-auto">
     <div
       class="mb-6 bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600"
     >
@@ -7,7 +7,10 @@
         class="container flex flex-wrap items-center justify-between mx-auto"
       >
         <button class="flex space-x-2 items-center">
-          <Icon class="text-2xl text-gray-900" name="ri:lightbulb-flash-fill" />
+          <Icon
+            class="text-lg text-gray-700"
+            name="ic:outline-arrow-back-ios"
+          />
           <span
             class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
           >
@@ -15,15 +18,13 @@
           </span>
         </button>
         <div class="flex md:order-2">
-          <button type="button" class="btn-primary">add feedback</button>
+          <button type="button" class="btn-primary">Edit feedback</button>
         </div>
       </div>
     </div>
 
     <FeedBackCard />
+    <CommentList />
+    <AddComment />
   </div>
 </template>
-<script>
-export default {};
-</script>
-<style lang=""></style>
