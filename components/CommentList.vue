938ce21726a4fb2e1d4774499df8b60bc2c7153b
ZinoChan <devzinochan@gmail.com>
2023-01-20 21:00:38 +0100

    CommentList ui

---
+++ components/CommentList.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-white rounded p-6">
+    <h4 class="font-bold">
+      2 Comments
+      <Comment />
+      <ReplyList />
+      <Comment />
+    </h4>
+  </div>
+</template>
