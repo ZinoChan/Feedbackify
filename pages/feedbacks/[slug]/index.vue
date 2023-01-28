0c6ee9ef0bbb72ed241fd0a6d9e8775870ae75fa
ZinoChan <devzinochan@gmail.com>
2023-01-28 22:26:52 +0100

    fetch project data

--- pages/feedbacks/[slug]/index.vue
+++ pages/feedbacks/[slug]/index.vue
@@ -2,16 +2,22 @@
   <div class="max-w-screen-lg mx-auto px-4 y-6">
     <div class="items-start grid grid-cols-4 gap-8">
       <div>
+        <h1>{{ slug }}</h1>
         <ProductCard />
         <CategoryList />
       </div>
       <div class="col-span-3">
         <FeedbackHeader />
-        <FeedBackCard />
-        <FeedBackCard />
-        <FeedBackCard />
-        <FeedBackCard />
+        <FeedBackCard
+          v-for="feedback in project.feedbacks"
+          :feedback="feedback"
+        />
       </div>
     </div>
   </div>
 </template>
+<script setup>
+const { slug } = useRoute().params;
+
+const { data: project } = await useFetch(`/api/project/${slug}`);
+</script>
