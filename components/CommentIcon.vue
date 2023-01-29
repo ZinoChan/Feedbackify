016088d926819853bb886daf3a329fc27c59b2b6
ZinoChan <devzinochan@gmail.com>
2023-01-29 22:12:56 +0100

    add comment data

--- components/CommentIcon.vue
+++ components/CommentIcon.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="flex space-x-2 items-center">
     <Icon class="text-xl text-gray-500" name="ant-design:comment-outlined" />
-    <span class="font-bold text-sm">4</span>
+    <span class="font-bold text-sm">
+      {{ commentsCount ? commentsCount : 0 }}
+    </span>
   </div>
 </template>
+
+<script setup>
+const { commentsCount } = defineProps(["commentsCount"]);
+</script>
