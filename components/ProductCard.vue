05ab32175638f48c32668b796bdb94ec8e5e35d1
ZinoChan <devzinochan@gmail.com>
2023-01-29 22:16:52 +0100

    minor change

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -3,8 +3,14 @@
     class="shadow-md shadow-teal-200 mb-6 rounded-md p-6 bg-gradient-to-r from-green-400 to-teal-400 text-white flex items-end"
   >
     <div>
-      <h3 class="font-bold text-xl mb-4">Lapino</h3>
+      <h3 class="font-bold text-xl mb-4">
+        {{ title }}
+      </h3>
       <span>feed back board</span>
     </div>
   </div>
 </template>
+
+<script setup>
+const { title } = defineProps(["title"]);
+</script>
