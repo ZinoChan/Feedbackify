55f6314309ddab01834b3d05fe7be5e8b3a03a0c
ZinoChan <devzinochan@gmail.com>
2023-01-29 22:14:29 +0100

    add vote data

--- components/Vote.vue
+++ components/Vote.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="rounded-lg bg-gray-50 p-2 flex flex-col justify-between">
-    <span class="font-bold text-sm text-teal-400">152</span>
+    <span class="font-bold text-sm text-teal-400">
+      {{ count ? count : 0 }}
+    </span>
   </div>
 </template>
+<script setup>
+const { count } = defineProps(["count"]);
+</script>
