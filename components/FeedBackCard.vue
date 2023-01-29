687988d1d6b4ec30297f22d6436853354efc030e
ZinoChan <devzinochan@gmail.com>
2023-01-29 22:13:44 +0100

    add feedback data

--- components/FeedBackCard.vue
+++ components/FeedBackCard.vue
@@ -1,24 +1,32 @@
 <template>
-  <div
-    class="mb-6 w-full p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
-  >
-    <div class="flex space-x-8 items-center justify-between">
-      <div class="flex items-center space-x-6">
-        <Vote />
-        <div>
-          <h5
-            class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white"
-          >
-            Add tags for solutions
-          </h5>
+  <NuxtLink :to="`/feedbacks/${params.slug}/${feedback?.id}`">
+    <div
+      class="mb-6 w-full p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700"
+    >
+      <div class="flex space-x-8 items-center justify-between">
+        <div class="flex items-center space-x-6">
+          <Vote :count="feedback?.upvotes" />
+          <div>
+            <h5
+              class="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white"
+            >
+              {{ feedback?.title }}
+            </h5>
 
-          <p class="text-sm mb-4 font-normal text-gray-700 dark:text-gray-400">
-            Easier to search for solutions based on a specific stack.
-          </p>
-          <Category category="enhancement" />
+            <p
+              class="text-sm mb-4 font-normal text-gray-700 dark:text-gray-400"
+            >
+              {{ feedback?.description }}
+            </p>
+            <Category :category="feedback?.category" />
+          </div>
         </div>
+        <CommentIcon :commentsCount="feedback?.comments?.length" />
       </div>
-      <CommentIcon />
     </div>
-  </div>
+  </NuxtLink>
 </template>
+<script setup>
+const { feedback } = defineProps(["feedback"]);
+const params = useRoute().params;
+</script>
