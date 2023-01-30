8c288d197f159b284a25e9ce8fb18ff310182591
ZinoChan <devzinochan@gmail.com>
2023-01-30 20:40:16 +0100

    clean up

--- pages/feedbacks/[slug]/index.vue
+++ pages/feedbacks/[slug]/index.vue
@@ -2,7 +2,6 @@
   <div class="max-w-screen-lg mx-auto px-4 y-6">
     <div class="items-start grid grid-cols-4 gap-8">
       <div>
-        <h1>{{ slug }}</h1>
         <ProductCard :title="project.name" />
         <CategoryList />
       </div>
