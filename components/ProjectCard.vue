d2c3e70b13af3d28af02979e9425e7e0b7e2c01b
ZinoChan <devzinochan@gmail.com>
2023-01-30 20:39:50 +0100

    minor changes

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -19,7 +19,9 @@
     <div class="mt-auto flex justify-between items-center">
       <NuxtLink :to="`/feedbacks/${project.slug}`">
         <div class="flex space-x-3 text-sm font-bold text-gray-600">
-          <span>34</span>
+          <span>
+            {{ project?.feedbacks?.length || 0 }}
+          </span>
           <span>feedbacks</span>
         </div>
       </NuxtLink>
