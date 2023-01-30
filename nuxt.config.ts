557b34b5d2b1627a609fa8d7a7f21dfe99414ce5
ZinoChan <devzinochan@gmail.com>
2023-01-30 20:35:43 +0100

    fix google fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
+  modules: [
+    "nuxt-icon",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        googleFonts: {
+          families: {
+            Jost: true,
+          },
+        },
+      },
+    ],
+
+    "@nuxtjs/tailwindcss",
+  ],
+  devServerHandlers: [],
 });
