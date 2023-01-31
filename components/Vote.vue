02600b3eed8bd960c0b4d9203b316943f2d3a01d
ZinoChan <devzinochan@gmail.com>
2023-01-31 18:18:17 +0100

    styles

--- components/Vote.vue
+++ components/Vote.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="rounded-lg bg-gray-50 p-2 flex flex-col justify-between">
-    <span class="font-bold text-sm text-teal-400">
+  <div class="rounded-lg bg-dark1 p-2 flex flex-col justify-between">
+    <span class="font-bold text-sm text-primary2">
       {{ count ? count : 0 }}
     </span>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2 class="font-bold">All Projects</h2>
+    <h2 class="font-bold text-white uppercase text-lg my-6">All Projects</h2>
     <div class="grid grid-cols-4 gap-6">
       <ProjectCard v-for="project in projects" :project="project" />
     </div>
