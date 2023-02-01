b1482995ba0395fe578c1bf28b8d38a144001059
ZinoChan <devzinochan@gmail.com>
2023-02-01 21:01:42 +0100

    styles

--- pages/feedbacks/[slug]/[feedbackId]/index.vue
+++ pages/feedbacks/[slug]/[feedbackId]/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="max-w-screen-md mx-auto">
+  <div class="mt-10 max-w-screen-md mx-auto">
     <div
-      class="mb-6 bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600"
+      class="mb-6 bg-dark2 rounded-md px-2 sm:px-4 py-2.5 border border-gray2"
     >
       <div
         class="container flex flex-wrap items-center justify-between mx-auto"
       >
         <button class="flex space-x-2 items-center">
           <Icon
-            class="text-lg text-gray-700"
+            class="text-lg text-gray-100"
             name="ic:outline-arrow-back-ios"
           />
           <span
-            class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"
+            class="self-center text-xl font-semibold whitespace-nowrap text-white"
           >
             back
           </span>
