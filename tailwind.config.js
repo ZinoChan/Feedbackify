7ec21a5f778d2394e3bf9e3d12d127a2c3eb34a5
ZinoChan <devzinochan@gmail.com>
2023-02-01 20:59:54 +0100

    styles: change colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,9 @@ module.exports = {
       colors: {
         primary1: "#4AF58A",
         primary2: "#33D7EE",
-        dark1: "#202225",
-        dark2: "#1d1a23",
+        dark1: "#16171B",
+        dark2: "#1E1F25",
+        dark3: "#2A2A2C",
         gray1: "#D9D5DC",
         gray2: "#393544",
       },
