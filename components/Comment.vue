3fc06c65ab6d063e614d28d6834f2025f001a557
ZinoChan <devzinochan@gmail.com>
2023-02-01 21:00:52 +0100

    styles

--- components/Comment.vue
+++ components/Comment.vue
@@ -1,19 +1,15 @@
 <template>
-  <article
-    class="border-b border-gray-100 p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900"
-  >
+  <article class="border-b border-gray2 p-6 mb-6 text-base bg-dark2">
     <footer class="flex justify-between items-center mb-2">
       <div class="flex items-center">
-        <p
-          class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white"
-        >
+        <p class="inline-flex items-center mr-3 text-sm text-white">
           <img
             class="mr-2 w-6 h-6 rounded-full"
             src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
             alt="Michael Gough"
           />{{ comment.user.name }}
         </p>
-        <p class="text-sm text-gray-600 dark:text-gray-400">
+        <p class="text-sm text-gray-400">
           <time pubdate datetime="2022-02-08" title="February 8th, 2022"
             >Feb. 8, 2022</time
           >
@@ -22,7 +18,7 @@
       <button
         id="dropdownComment1Button"
         data-dropdown-toggle="dropdownComment1"
-        class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-300 bg-dark1 rounded-lg hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-600"
         type="button"
       >
         <Icon class="text-lg" name="humbleicons:dots-horizontal" />
@@ -31,7 +27,7 @@
       <!-- Dropdown menu -->
       <div
         id="dropdownComment1"
-        class="hidden z-10 w-36 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+        class="hidden z-10 w-36 bg-dark1 rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
       >
         <ul
           class="py-1 text-sm text-gray-700 dark:text-gray-200"
@@ -61,15 +57,15 @@
         </ul>
       </div>
     </footer>
-    <p class="text-gray-500 text-sm dark:text-gray-400">
+    <p class="text-sm text-gray-200">
       {{ comment.content }}
     </p>
     <div class="flex items-center mt-4 space-x-4">
       <button
         type="button"
-        class="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
+        class="flex items-center text-sm text-gray-200 hover:underline"
       >
-        <Icon class="text-lg mr-1 text-gray-700" name="uil:comment-dots" />
+        <Icon class="text-lg mr-1 text-gray-400" name="uil:comment-dots" />
         Reply
       </button>
     </div>
