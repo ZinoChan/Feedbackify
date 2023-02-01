50045cc413b97fe67b025ccdc85d76cb043e79b2
ZinoChan <devzinochan@gmail.com>
2023-02-01 21:01:21 +0100

    styles

--- components/CommentList.vue
+++ components/CommentList.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="bg-white rounded p-6">
-    <h4 class="font-bold">
+  <div class="bg-dark2 border border-gray2 rounded p-6">
+    <h4 class="font-bold text-white mb-4">
       {{ comments?.length || 0 }} Comments
-
-      <Comment v-for="(comment, id) in comments" :key="id" :comment="comment" />
     </h4>
+
+    <Comment v-for="(comment, id) in comments" :key="id" :comment="comment" />
   </div>
 </template>
 
