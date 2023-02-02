20f26c87a8070ccb64d4dea41ecb7d138c522b05
ZinoChan <devzinochan@gmail.com>
2023-02-02 17:24:43 +0100

    set category

--- components/Category.vue
+++ components/Category.vue
@@ -1,12 +1,14 @@
 <template>
-  <span
+  <button
     :class="category"
+    @click="setCategory(category)"
     class="text-xs font-bold capitalize bg-opacity-10 mr-2 px-2 py-1 rounded-md border"
   >
     {{ category }}
-  </span>
+  </button>
 </template>
 
 <script setup>
+const { setCategory } = useCategory();
 const { category } = defineProps(["category"]);
 </script>
