c10144991f9e5ec567042065dcd08e714fcac92b
ZinoChan <devzinochan@gmail.com>
2023-02-02 17:26:20 +0100

    add query filter

--- server/api/project/[slug]/index.ts
+++ server/api/project/[slug]/index.ts
@@ -1,7 +1,15 @@
 import data from "~~/data";
 
 export default defineEventHandler((event) => {
+  const { category } = getQuery(event);
   const project = data.find((el) => el.slug === event.context.params.slug);
-  if (project) return project;
+
+  if (project)
+    return {
+      project,
+      feedbacks: category
+        ? project.feedbacks.filter((f) => f.category === category)
+        : project.feedbacks,
+    };
   else throw new Error("project, not found");
 });
