5779915c5e071e5ef56f28a63d10dd50411d31bf
ZinoChan <devzinochan@gmail.com>
2023-02-02 17:25:04 +0100

    clear category filter

--- components/CategoryList.vue
+++ components/CategoryList.vue
@@ -2,6 +2,12 @@
   <div class="bg-dark2 border border-gray2 rounded-lg p-6 shadow-md">
     <div class="flex gap-y-3 flex-wrap align-baseline">
       <Category v-for="cat in categories" :category="cat" />
+      <button
+        @click="setCategory('')"
+        class="text-xs text-white bg-white border-white font-bold capitalize bg-opacity-10 mr-2 px-2 py-1 rounded-md border"
+      >
+        All
+      </button>
     </div>
   </div>
 </template>
@@ -14,4 +20,7 @@ export default {
     };
   },
 };
+</script>
+<script setup lang="ts">
+const { setCategory } = useCategory();
 </script>
