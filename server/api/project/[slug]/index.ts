9b9c2d599fd99567e7a1fd0845ca8ab73579ef76
ZinoChan <devzinochan@gmail.com>
2023-02-03 21:02:50 +0100

    refactor

--- server/api/project/[slug]/index.ts
+++ server/api/project/[slug]/index.ts
@@ -1,15 +1,18 @@
-import data from "~~/data";
+import { feedbacks } from "~~/data/feedbacks";
+import { projects } from "~~/data/projects";
 
 export default defineEventHandler((event) => {
   const { category } = getQuery(event);
-  const project = data.find((el) => el.slug === event.context.params.slug);
+  const { slug } = event.context.params;
+  const project = projects[slug];
+  let $feedbacks = feedbacks[slug];
 
   if (project)
     return {
       project,
       feedbacks: category
-        ? project.feedbacks.filter((f) => f.category === category)
-        : project.feedbacks,
+        ? $feedbacks?.filter((f) => f.category === category)
+        : $feedbacks,
     };
   else throw new Error("project, not found");
 });
