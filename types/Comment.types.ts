196d7f9c36af681495f2d4d642de907f6e3276be
ZinoChan <devzinochan@gmail.com>
2023-02-04 19:08:24 +0100

    refactor types

--- types/Comment.types.ts
+++ types/Comment.types.ts
@@ -1,7 +1,18 @@
 import { IUser } from "./User.types";
 
+export interface IComments {
+  [slugId: string]: IComment[];
+}
+
 export interface IComment {
   id: number;
   content: string;
+  user: IUser;
+  replies?: IReply[];
+}
+
+export interface IReply {
+  content: string;
+  replyingTo: string;
   user: IUser;
 }
