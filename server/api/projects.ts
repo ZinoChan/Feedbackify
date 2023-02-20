c2e7a9d3497d170571a7e58a9b08fd38633caf50
ZinoChan <devzinochan@gmail.com>
2023-02-20 22:01:42 +0100

    fetch data with prisma

--- server/api/projects.ts
+++ server/api/projects.ts
@@ -1,3 +1,13 @@
-import { projects } from "~~/data/projects";
+import prisma from "~~/lib/prismaClient";
 
-export default defineEventHandler(() => Object.values(projects));
+export default defineEventHandler(async () => {
+  const data = await prisma.projects.findMany();
+
+  const projects = data.map((val) =>
+    JSON.stringify(val, (key, value) =>
+      typeof value === "bigint" ? `BIGINT::${value}` : value
+    )
+  );
+
+  return projects;
+});
